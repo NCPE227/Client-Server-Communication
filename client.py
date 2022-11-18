a25d6ef3b1779e649bdb6d5c305cf07a8c9f6588
NCPE227 <perrynoahchristian@gmail.com>
2022-11-18 11:05:57 -0500

    Upload and Download Functionality
    Needs testing, particularly on different machines. Also, needs multi-threading

--- client.py
+++ client.py
@@ -1,61 +1,70 @@
-#client-one.py
 import socket
+from tqdm import tqdm
 import os
 
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096 #Send 4096 bytes per step
 HOST = '127.0.0.1' #hostname
 PORT = 65432  #server port
-path = 'C:\\Users\\perry\\Documents\\GitHub\\Client-Server-Communication'
 
-#Set up a system to ask the user to make a request
+#Create socket for client and connect to host and port
+client = socket.socket()
+client.connect((HOST, PORT))
+print('Connected to ', HOST, ' : ', PORT, ' Successfully')
 
-def Communication(query):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-        client.connect((HOST, PORT))
+#Ask the user to request one of the program's functions
+function = input('Upload File, Download File, or Quit? ').lower()
 
-        #Upload a file to the server
-        if query == 'upload':
-            print('You selected upload.')
-            filename = input('Input desired filename to upload to server: ')
+#Program will quit if the users enters quit when selecting a function
+while function != 'quit':
+    if function == 'upload':
+        print(f'You chose {function}.')
+        
+        #Ask for filename and get the size
+        filename = input('Insert the name of the file you wish to upload: ')
+        filesize = os.path.getsize(filename) #This would be handled on the server side for download
+        
+        #Send the function request, filename, and filesize to the server
+        client.send(f"{function}{SEPARATOR}{filename}{SEPARATOR}{filesize}".encode())
 
-            #Get file from computer and send it to the server.
-            client.send(filename.encode('utf-8'))
-            filepath = os.path.join(path, filename)
-            filepath = os.path.join(path, filename)
-            f = open(filepath, 'rb')
-            l = f.read(1024)
-            while l:
-                client.send(l)
-                l = f.read(1024)
-            f.close()
-
-        #Download a file from the server
-        elif query == 'download':
-            print('You selected download.')
-            filename = input('Input desired filename to download from server: ')
-
-            #Send requested filename to server for download
-            client.send(filename.encode('utf-8'))
-            filepath = os.path.join(path, filename)
-            f = open(filepath, 'ab')
+        #Start sending the file
+        progress = tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024) 
+        with open(filename, "rb") as f:
             while True:
-                data = client.recv(1024)
-                if not data:
+                #Read bytes from the file
+                bytes_read = f.read(BUFFER_SIZE)
+                if not bytes_read: #no more bytes to read
                     break
-                f.write(data)
-            f.close()
+    
+                client.sendall(bytes_read)
+                
+                #Update the progress bar
+                progress.update(len(bytes_read))
+        data = client.recv(BUFFER_SIZE) #get an acknowledgement message from the server
 
-while True:
-    print('Upload or Download?')
-    query = input('Enter an option: ').lower()
-
-    if (query == 'upload') | (query == 'download'):
-        Communication(query)
-        break
+    elif function == 'download':
+        print('You chose {}.').format(function)
+        filename = input('Insert the name of the file you wish to download: ')
+        data = client.recv(BUFFER_SIZE).decode() #receive the filename and filesize of the desired file in the server
+        progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        
+        #Open file and write it to the local directory
+        with open(filename, "wb") as f:
+            while True:
+                # read 1024 bytes from the socket (receive)
+                bytes_read = client.recv(BUFFER_SIZE)
+                if not bytes_read:    
+                    # nothing is received
+                    # file transmitting is done
+                    break
+                # write to the file the bytes we just received
+                f.write(bytes_read)
+                # update the progress bar
+                progress.update(len(bytes_read))
 
     else:
-        print('\"', query, '\" is not an acceptable option.')
-        print('Please select from Upload or Download.')
-
-            
-
-
+        print('Sorry, {} is not a supported request.').format(function)
+        function = input('Upload File, Download File, or Quit? ').lower()
+        
+client.close() #close the client socket
+        
--- server.py
+++ server.py
@@ -1,16 +1,63 @@
 import socket
+import os
+import tqdm
 
-HOST = "127.0.0.1"  #localhost
-PORT = 65432  #port server is listening to
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096 #Send 4096 bytes per step
+HOST = '127.0.0.1' #hostname
+PORT = 65432  #server port
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.bind((HOST, PORT))
-    server.listen()
-    connection, address = server.accept()
-    with connection:
-        print(f"Connected by {address}")
+#Initialize server socket
+server = socket.socket()
+server.bind((HOST, PORT)) #bind server host and port
+server.listen(5)
+print(f"[*] Listening as {HOST}:{PORT}") #printout to confirm connection
+
+#Accept the connection from a client
+connection, address = server.accept()
+#Printout to confirm connection
+print(f"[+] {address} is connected.")
+
+#Use client's socket to receive the decoded file
+received = connection.recv(BUFFER_SIZE).decode()
+request, filename, filesize = received.split(SEPARATOR)
+filename = os.path.basename(filename) #Remove absolute path
+filesize = int(filesize) #integer conversion for filesize, due to encoding/decoding
+
+#Handle 2 different request options
+if request == 'upload': #upload will take a file from the client and write it to the server
+    #Receive a file from the client
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "wb") as f:
         while True:
-            data = connection.recv(1024)
-            if not data:
+            # read 1024 bytes from the socket (receive)
+            bytes_read = connection.recv(BUFFER_SIZE)
+            if not bytes_read:    
+                # nothing is received
+                # file transmitting is done
                 break
-            connection.sendall(data)
+            f.write(bytes_read) #write the file
+            progress.update(len(bytes_read)) #update our progress bar
+
+elif request == 'download': #download will send a file to the client and allow it to write
+        
+        #Send the filename and filesize to the client
+        server.send(f"{SEPARATOR}{filename}{SEPARATOR}{filesize}".encode())
+
+        #Start sending the file
+        progress = tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024) 
+        with open(filename, "rb") as f:
+            while True:
+                #Read bytes from the file
+                bytes_read = f.read(BUFFER_SIZE)
+                if not bytes_read: #no more bytes to read
+                    break
+    
+                server.sendall(bytes_read)
+                
+                #Update the progress bar
+                progress.update(len(bytes_read))
+        data = server.recv(BUFFER_SIZE) #get an acknowledgement message from the server
+
+else:
+    server.send('Sorry, that was a bad request. Please try again.')
